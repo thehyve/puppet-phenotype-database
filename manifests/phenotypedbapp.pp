a23ab2f4756f08efd15b354b44245b13c2d3c6c0
sethsnel <seth@thehyve.nl>
2013-07-08 10:38:37 +0000

    Updated Phenotype Database puppy script to retrieve latest war from Bamboo.

--- manifests/phenotypedbapp.pp
+++ manifests/phenotypedbapp.pp
@@ -48,7 +48,7 @@ define phenotypedb::phenotypedbapp (
     $deployment_dir = "$tomcat_home/tomcat/webapps"
     $downloaded_war = "$temporary_dir/gscf-${instancename}.war"
     $deployed_war   = "$deployment_dir/gscf-${instancename}.war"
-    $download_url   = "https://trac.nbic.nl/gscf/downloads/${phenotypedbwarid}"    
+    $download_url   = "https://ci.ctmmtrait.nl/browse/PD-PDM/latest/artifact/shared/Phenotype-Database-WAR/gscf-0.9.1.3.war"    
         
     tomcat::webapp { $system_user:
         username        => $system_user,   # info: the tomcat::webapp script already ensures user is created as well 
